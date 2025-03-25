01f466161514db56c9bd3cf44e28ea39799ddcf2
Helen Goatly <hgoatly@hotmail.com>
2025-03-25 13:36:32 +0000

    Chore: Update vite.config.js and package.json.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/helen_manente_yoga/", // Replace with your actual GitHub repo name
+  plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
+});
