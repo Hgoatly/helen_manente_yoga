34fd9910cb44c8def5c3e10fed96c1e2eb93fc40
Helen Goatly <hgoatly@hotmail.com>
2025-03-25 13:57:37 +0000

    Fix: Update vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// filepath: c:\Users\hgoat\helen_manente_yoga\vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   base: "/helen_manente_yoga/", // Replace with your actual GitHub repo name
-  plugins: [react(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react()],
 });
